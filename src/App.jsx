f788bf08b3c38d9eef23eb43c1fbdc9300c58339
Harsh Vaghani <harshvaghani98@gmail.com>
2023-01-25 10:17:01 +0530

    Completing Work section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
 import Experience from "./components/Experience/Experience";
+import Works from "./components/Works/Works";
+import Portfolio from "./components/Portfolio/Portfolio";
 const App = () => {
     return (
         <div className="App">
@@ -11,6 +13,8 @@ const App = () => {
             <Intro />
             <Services />
             <Experience />
+            <Works />
+            <Portfolio />
         </div>
     );
 };
---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,62 @@
+import "./Works.css";
+import React from "react";
+import Upwork from "../../img/Upwork.png";
+import Fiverr from "../../img/fiverr.png";
+import Amazon from "../../img/amazon.png";
+import Shopify from "../../img/Shopify.png";
+import Facebook from "../../img/Facebook.png";
+
+const Works = () => {
+    return (
+        <div className="works">
+            {/* Left Side */}
+
+            <div className="awesome">
+                <span>Works for All these</span>
+                <span>Brands & Clients</span>
+                <span>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing.
+                    <br />
+                    Lorem ipsum, dolor sit amet consectetur adipisicing.
+                    <br />
+                    dolor sit amet consectetur adipisicing.
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing.
+                </span>
+                <button className="button s-button">Hire Me</button>
+                <div
+                    className="blur s-blur"
+                    style={{
+                        background: "#ABF1FF94",
+                    }}
+                ></div>
+            </div>
+
+            {/* Right Side */}
+            <div className="w-right">
+                <div className="w-mainCircle">
+                    <div className="w-secCircle">
+                        <img src={Upwork} alt="Upwork Logo" />
+                    </div>
+                    <div className="w-secCircle">
+                        <img src={Fiverr} alt="Fiverr Logo" />
+                    </div>
+                    <div className="w-secCircle">
+                        <img src={Amazon} alt="Amazon Logo" />
+                    </div>
+                    <div className="w-secCircle">
+                        <img src={Shopify} alt="Shopify Logo" />
+                    </div>
+                    <div className="w-secCircle">
+                        <img src={Facebook} alt="Facebook Logo" />
+                    </div>
+                </div>
+                {/* Background Circles */}
+                <div className="w-backCircle blueCircle"></div>
+                <div className="w-backCircle yellowCircle"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Works;
